25e3af659bfc2641924bc8cdcac332f3c0c3fc3d
KaranModi28 <kpmodi28@gmail.com>
2025-01-20 13:38:15 -0500

    Added react native to reportanitem
    Added react native to reportanitem

--- reportanitem.js
+++ reportanitem.js
@@ -1,2 +1,16 @@
-//HTML 
+import React from 'react'; //imports react
+import {Text, View} from 'react-native';
 
+const ReportItem = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Report an Item? Test</Text> 
+    </View>
+  );
+};
+export default ReportItem;

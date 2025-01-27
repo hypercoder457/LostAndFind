424cd739afc7b21b7446862fda5f08fcd2e7981a
RealRottale1 <camarkham2@gmail.com>
2025-01-27 17:54:19 -0500

    Minor Update! | Made structure for DB
    - Made structure for DB

--- Database.js
+++ Database.js
@@ -2,6 +2,125 @@ import { getDatabase, ref, set, update, push, remove, onValue } from "firebase/d
 import getFirebaseApp from "./firebaseConfig.js";
 
 const db = getDatabase(getFirebaseApp());
+const counties = [
+    "alamance",
+    "alexander",
+    "alleghany",
+    "anson",
+    "ashe",
+    "avery",
+    "beaufort",
+    "beaufort",
+    "bertie",
+    "bladen",
+    "brunswick",
+    "buncombe",
+    "burke",
+    "cabarrus",
+    "caldwell",
+    "camden",
+    "carteret",
+    "caswell",
+    "catawba",
+    "chatham",
+    "cherokee",
+    "chowan",
+    "clay",
+    "cleveland",
+    "columbus",
+    "craven",
+    "cumberland",
+    "currituck",
+    "dare",
+    "davidson",
+    "davie",
+    "duplin",
+    "durham",
+    "edgecombe",
+    "forsyth",
+    "franklin",
+    "gaston",
+    "gates",
+    "graham",
+    "granville",
+    "greene",
+    "guilford",
+    "halifax",
+    "harnett",
+    "haywood",
+    "henderson",
+    "hertford",
+    "hoke",
+    "hyde",
+    "iredell",
+    "jackson",
+    "johnston",
+    "jones",
+    "lee",
+    "lenoir",
+    "lincoln",
+    "macon",
+    "madison",
+    "martin",
+    "mcdowell",
+    "mecklenburg",
+    "mitchell",
+    "montgomery",
+    "moore",
+    "nash",
+    "new hanover",
+    "northampton",
+    "onslow",
+    "orange",
+    "pamlico",
+    "pasquotank",
+    "pender",
+    "perquimans",
+    "person",
+    "pitt",
+    "polk",
+    "randolph",
+    "richmond",
+    "robeson",
+    "rockingham",
+    "rowan",
+    "sampson",
+    "scotland",
+    "stanly",
+    "stokes",
+    "surry",
+    "swain",
+    "transylvania",
+    "tyrrell",
+    "union",
+    "vance",
+    "wake",
+    "warren",
+    "washington",
+    "watauga",
+    "wayne",
+    "wilkes",
+    "wilson",
+    "yadkin",
+    "yancey",
+  ];
+  const categories = [
+    "clothing", "bag", "electronic", "key", "wallet", "jewelry", "bottle", "book", "important documentation", "other"
+  ];
+const totalCounties = counties.length;
+const totalCategories = categories.length;
+const mainPath = "reports/";
+for (let i = 0; i < totalCounties; i++) {
+    const currentCounty = counties[i];
+    for (let j = 0; j < totalCategories; j++) {
+        const currentCategory = categories[j];
+        const usePath = mainPath + currentCounty + "/" + currentCategory; 
+        const newRef = ref(db, usePath);
+        set(newRef, "");
+    }
+}
+console.log("Finished Creating Database Structure!");
+/*
 const ref1 = ref(db, "Alamance/Clothing");
 const clothingRef = push(ref1);
 update(clothingRef, { item: "Shirt", color: "Blue", size: "Large" });
@@ -9,3 +128,4 @@ remove(ref(db, "Alamance/Clothing/3"));
 onValue(ref(db, "Alamance/Clothing"), (snapshot) => {
     console.log(snapshot.val());
 });
+*/
